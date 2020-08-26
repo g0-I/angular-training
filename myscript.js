1af711694d0c5c5b6f843c97b5fba142da8b77cd
0k1ta <freeer55@gmail.com>
2020-08-26 23:54:54 +0900

    ng-repeat内で使えるフィルターの追加

--- myscript.js
+++ myscript.js
@@ -4,6 +4,12 @@ angular.module('myapp', [])
           {"name": "tanaka", "score":43.65},
           {"name": "sato", "score":84.32},
           {"name": "kobayashi", "score":67.89},
+          {"name": "tanaka2", "score":43.65},
+          {"name": "sato2", "score":84.32},
+          {"name": "kobayashi2", "score":67.89},
+          {"name": "tanaka3", "score":43.65},
+          {"name": "sato3", "score":84.32},
+          {"name": "kobayashi3", "score":67.89},
       ];
       $scope.today = new Date();
   }]);
