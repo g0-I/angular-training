6721aa9bdb09da70f18780232e4dfa65b8032c8a
okita <59220768+0k1ta@users.noreply.github.com>
2020-09-02 00:02:13 +0900

    ループを入れ子にしたときの挙動とクリックイベントを追加 (#7)
    * ng-repeat内のフィルターを追加
    
    * コントローラーのイベントを追加

--- myscript.js
+++ myscript.js
@@ -1,14 +1,18 @@
 angular.module('myapp', [])
     .controller('MainController', ['$scope', function ($scope) {
         $scope.users = [
-            { "name": "tanaka", "score": 43.65 },
-            { "name": "sato", "score": 84.32 },
-            { "name": "kobayashi", "score": 67.89 },
-            { "name": "tanaka2", "score": 43.65 },
-            { "name": "sato2", "score": 84.32 },
-            { "name": "kobayashi2", "score": 67.89 },
-            { "name": "tanaka3", "score": 43.65 },
-            { "name": "sato3", "score": 84.32 },
-            { "name": "kobayashi3", "score": 67.89 },
+            { "name": "sato", "score": 52.22 },
+            { "name": "tanaka", "score": 38.22 },
+            { "name": "yamada", "score": 11.11 },
+            { "name": "hayashi", "score": 5.25 },
+            { "name": "higuchi", "score": 82.4 },
+            { "name": "yasuda", "score": 55.21 },
+            { "name": "minami", "score": 32.8 },
+            { "name": "yanagi", "score": 72.2 }
         ];
+    }])
+    .controller('UserItemController', ['$scope', function ($scope) {
+        $scope.increment = function () {
+            $scope.user.score++;
+        };
     }]);
