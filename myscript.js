6ff8b219f0f16caf5e36718f76947e156d68b338
okita <59220768+0k1ta@users.noreply.github.com>
2020-09-03 23:46:33 +0900

    送信フォームを追加 (#8)
    * ng-repeat内のフィルターを追加
    
    * コントローラーのイベントを追加
    
    * Add submit form

--- myscript.js
+++ myscript.js
@@ -1,18 +1,4 @@
-angular.module('myapp', [])
-    .controller('MainController', ['$scope', function ($scope) {
-        $scope.users = [
-            { "name": "sato", "score": 52.22 },
-            { "name": "tanaka", "score": 38.22 },
-            { "name": "yamada", "score": 11.11 },
-            { "name": "hayashi", "score": 5.25 },
-            { "name": "higuchi", "score": 82.4 },
-            { "name": "yasuda", "score": 55.21 },
-            { "name": "minami", "score": 32.8 },
-            { "name": "yanagi", "score": 72.2 }
-        ];
-    }])
-    .controller('UserItemController', ['$scope', function ($scope) {
-        $scope.increment = function () {
-            $scope.user.score++;
-        };
-    }]);
+var MainController = function ($scope) {
+    $scope.addUser = function () {
+    }
+}
