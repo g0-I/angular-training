9723fdcf8d41dd5274b4cebab3be3921f6f67dc3
okita <59220768+0k1ta@users.noreply.github.com>
2020-09-09 23:27:54 +0900

    [V1.0]基本的なフィルター等の使い方確認 (#12)
    * データ表示のフィルターを追加 (#3)
    
    * データ表示のフィルターを追加 (#4)
    
    * ng-repeat内のフィルターの追加 (#5)
    
    * データ表示のフィルターを追加
    
    * ng-repeat内で使えるフィルターの追加
    
    * ng-repeat内の命令を追加 (#6)
    
    * データ表示のフィルターを追加
    
    * ng-repeat内で使えるフィルターの追加
    
    * add method
    
    * ループを入れ子にしたときの挙動とクリックイベントを追加 (#7)
    
    * ng-repeat内のフィルターを追加
    
    * コントローラーのイベントを追加
    
    * 送信フォームを追加 (#8)
    
    * ng-repeat内のフィルターを追加
    
    * コントローラーのイベントを追加
    
    * Add submit form
    
    * バリデーションの追加 (#9)
    
    * ng-repeat内のフィルターを追加
    
    * コントローラーのイベントを追加
    
    * Add submit form
    
    * バリデーションの追加
    
    * フォームの追加
    
    * リストボックスの追加 (#10)
    
    * ng-repeat内のフィルターを追加
    
    * フォームの部品を設置
    
    * リストボックスの設置
    
    * リストボックスの設置 (#11)
    
    * ng-repeat内のフィルターを追加
    
    * フォームの部品を設置
    
    * リストボックスの設置

--- myscript.js
+++ myscript.js
@@ -1,8 +1,4 @@
-angular.module('myapp', [])
-  .controller('MainController', ['$scope', function($scope){
-      $scope.users = [
-          {"name": "tanaka", "score":43.65},
-          {"name": "sato", "score":84.32},
-          {"name": "kobayashi", "score":67.89},
-      ];
-  }]);
+var mainCtrl = function ($scope) {
+    $scope.addUser = function () {
+    }
+}
